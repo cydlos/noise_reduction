9c50a891bb2740604c59cf1a340cc9cf3ae03b8d
cyd losekann <cydnel@gmail.com>
2024-02-23 03:27:04 -0300

    identação

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ file = input("Enter the file name: ")
 sr, data = wav.read(file)
 fl = 400
 frames = []
-for i in range(0, int (len(data)/fl)):
-    arr = np.array(data[i*fl:(i+1)*fl])
+for i in range(0, int(len(data) / fl)):
+    arr = np.array(data[i * fl:(i + 1) * fl])
     frames.append(arr)
+
 frames = np.array(frames)
 ham_window = np.hamming(fl)
-windowed_frames = frames*ham_window
+windowed_frames = frames * ham_window
 dft = []
 
 for i in windowed_frames:
@@ -19,16 +20,22 @@ for i in windowed_frames:
 
 dft = np.array(dft)
 dft_mag_spec = np.abs(dft)
-dft_phase_spec = np.angle
-noise_estimate = np.mean(dft.mag_spec, axis=0)
+dft_phase_spec = np.angle(dft)
+noise_estimate = np.mean(dft_mag_spec, axis=0)
 noise_estimate_mag = np.abs(noise_estimate)
-estimate_mag = (dft_mag_spec - 2*noise_estimate_mag)
+estimate_mag = dft_mag_spec - 2 * noise_estimate_mag
 estimate_mag[estimate_mag < 0] = 0
-estimate = estimate_mag * np.exp(1j*dft_phase_spec)
+estimate = estimate_mag * np.exp(1j * dft_phase_spec)
 ift = []
 
 for i in estimate:
     ift.append(np.fft.ifft(i))
 
 clean_data = []
-clean_data.extend(ift[0][:int(fl/2)])
+clean_data.extend(ift[0][:int(fl / 2)])
+for i in range(1, len(ift) - 1):
+    clean_data.extend(ift[i][int(fl / 2):int(fl)])
+    clean_data.extend(ift[i + 1][:int(fl / 2)])
+    clean_data = np.array(clean_data)
+
+fig = plt.figure(figsize=(8, 5))
